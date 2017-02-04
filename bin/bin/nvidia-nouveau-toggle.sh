d2406786cd553b29252a743aaa06e07e274e5183
GRBurst <JuliusE89@gmail.com>
2017-02-04 10:42:42 +0100

    nvidia-mesa optimization

--- bin/bin/nvidia-nouveau-toggle.sh
+++ bin/bin/nvidia-nouveau-toggle.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-BRANCH=-grsec # Enter a branch if needed, i.e. -340xx or -304xx
+BRANCH= # Enter a branch if needed, i.e. -340xx or -304xx
 NVIDIA=nvidia${BRANCH} # If no branch entered above this would be "nvidia"
 NOUVEAU=xf86-video-nouveau
 place=$(pwd)
 userhome=/home/jelias/dotfiles
 
 # Replace -R with -Rs to if you want to remove the unneeded dependencies
-if [ $(yaourt -Qqs ^mesa-libgl$) ]; then
-    yaourt -S $NVIDIA --noconfirm
-    # yaourt -S $NVIDIA ${NVIDIA}-libgl lib32-${NVIDIA}-libgl # Add lib32-${NVIDIA}-libgl and ${NVIDIA}-lts if needed
-    # yaourt -R $NOUVEAU
-    cd $userhome
-    stow -D compton-mesa
-    stow compton-nvidia
-    cd $place
-elif [ $(yaourt -Qqs ^${NVIDIA}$) ]; then
+#if [ $(yaourt -Qqs ^mesa-libgl$) ]; then
+#    yaourt -S $NVIDIA --noconfirm
+#    # yaourt -S $NVIDIA ${NVIDIA}-libgl lib32-${NVIDIA}-libgl # Add 
+lib32-${NVIDIA}-libgl and ${NVIDIA}-lts if needed
+#    # yaourt -R $NOUVEAU
+#    cd $userhome
+#    stow -D compton-mesa
+#    stow compton-nvidia
+#    cd $place
+#elif [ $(yaourt -Qqs ^${NVIDIA}$) ]; then
     yaourt -S --needed $NOUVEAU mesa-libgl lib32-mesa-libgl --noconfirm
     yaourt -R $NVIDIA # Add ${NVIDIA}-lts if needed
     cd $userhome
     stow -D compton-nvidia
     stow compton-mesa
     cd $place
-fi
+#fi
