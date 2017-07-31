765af56894d2e00bf74fdf9dc0aef0335be00fea
GRBurst <JuliusE89@gmail.com>
2017-07-31 20:09:59 +0200

    i3: remove programs being started on different places

--- .config/i3/common/start.sh
+++ .config/i3/common/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 feh --bg-scale '/home/jelias/.config/i3/background0.jpg' '/home/jelias/.config/i3/background1.jpg' &
-unclutter &
-nm-applet &
-pasystray &
-redshift-gtk &
+# nm-applet &
+# pasystray &
+# unclutter &
+# redshift-gtk &
