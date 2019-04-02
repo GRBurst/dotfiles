f748f5bc98fbbd6d97b1c9cdc40c6393d1cb0217
GRBurst <GRBurst@protonmail.com>
2019-04-02 19:05:32 +0200

    Update nixos config of mars

--- projects/nixos/config.mars.nix
+++ projects/nixos/config.mars.nix
@@ -230,7 +230,7 @@ in {
 
       paths = [
         brasero
-        (chromium.override { enablePepperFlash = true; enableWideVine = false;})
+        (chromium.override { enablePepperFlash = false; enableWideVine = false;})
         clementine
         cryptsetup
         evince
