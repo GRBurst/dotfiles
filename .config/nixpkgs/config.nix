1ee7721c5fe59ead8063fd26bc0824d5b20df4ad
GRBurst <GRBurst@protonmail.com>
2019-08-05 09:29:56 +0200

    add several packages for pdfs

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -43,7 +43,7 @@ in {
         wget netcat nmap
         psmisc
         hdparm hd-idle hddtemp
-        pv xclip xorg.xkill unclutter-xfixes
+        pv xclip xorg.xkill unclutter-xfixes xwininfo
         lm_sensors calc gksu
         haskellPackages.yeganesh
         gnumake
@@ -60,7 +60,7 @@ in {
         p7zip
         speedtest-cli
         traceroute
-        unzip
+        zip unzip
         xcwd
         xorg.xdpyinfo
         xorg.xev
@@ -114,6 +114,8 @@ in {
         texlive.combined.scheme-full
         # biber # collides texlive full
         pdftk #pdfshuffler
+        pdfsandwich
+        tesseract
         poppler_utils
         xournal
 
@@ -258,6 +260,7 @@ in {
         josm
         peek # record gif videos || green-recorder / gifcurry / screenToGif
         kodi
+        linphone ekiga
         openjdk
         protonmail-bridge
         qutebrowser
@@ -265,7 +268,7 @@ in {
         skypeforlinux
         shotwell
         texmaker texstudio #lyx
-        tor-browser-bundle-bin # prevented highres from upgrade
+        tor-browser-bundle-bin
         thunderbird
         vlc
         vokoscreen keymon
