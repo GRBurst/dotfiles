0a0f6b35030e2a3c8663f26acb5ab6c00eaf3fcc
GRBurst <GRBurst@protonmail.com>
2020-07-08 12:25:05 +0200

    common packages: remove typora (breaks)
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -127,7 +127,7 @@ in {
         libnotify
         qrencode
         simple-scan
-        typora
+        # typora # breaks on 2020-07-08
         zathura
         texlive.combined.scheme-full
         thunderbird protonmail-bridge
