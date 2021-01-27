7f3af1615a5e7a224f6c13201526173a67f1bd11
GRBurst <GRBurst@protonmail.com>
2021-01-27 20:13:03 +0100

    add etesync-dav on start
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- .config/i3/common/start.sh
+++ .config/i3/common/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 feh --bg-scale '/home/jelias/.config/i3/background0.jpg' '/home/jelias/.config/i3/background1.jpg' &
+ETESYNC_URL=https://scal.metacosmos.space etesync-dav &
 # nm-applet &
 # pasystray &
 # unclutter &
