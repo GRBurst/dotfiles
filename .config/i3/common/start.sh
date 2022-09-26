d752985b43588c74fdafde6fdfa217bfc08eedd6
GRBurst <GRBurst@protonmail.com>
2022-09-26 09:22:20 +0200

    Add protonvpn-cli to i3 startup but comment out (not working 100%)

--- .config/i3/common/start.sh
+++ .config/i3/common/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-feh --bg-scale '/home/jelias/.config/i3/background0.jpg' '/home/jelias/.config/i3/background1.jpg' &
 ETESYNC_URL=https://scal.metacosmos.space etesync-dav &
+# protonvpn-cli connect --sc
+# feh --bg-scale '/home/jelias/.config/i3/background0.jpg' '/home/jelias/.config/i3/background1.jpg' &
 # nm-applet &
 # pasystray &
 # unclutter &
