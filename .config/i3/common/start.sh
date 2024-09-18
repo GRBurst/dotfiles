0f95e92766d4be867b6a05163b261214c00793a6
GRBurst <GRBurst@protonmail.com>
2024-09-18 10:11:18 +0200

    i3: replace protonvpn-cli with protonvpn-app and remove feh
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- .config/i3/common/start.sh
+++ .config/i3/common/start.sh
@@ -2,9 +2,8 @@
 ETESYNC_URL=https://scal.metacosmos.space etesync-dav &
 qsyncthingtray &
 nm-applet &
-protonvpn-cli connect --sc
-protonvpn &
-# feh --bg-scale '/home/jelias/.config/i3/background0.jpg' '/home/jelias/.config/i3/background1.jpg' &
 # pasystray &
+protonvpn-app &
+protonmail-bridge -n &
 # unclutter &
 # redshift-gtk &
