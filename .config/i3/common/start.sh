153f73fce897a958922d7724d9ce35833ab504d6
GRBurst <GRBurst@protonmail.com>
2026-04-30 15:41:03 +0000

    i3: remove non-nixos configs
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- .config/i3/common/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ETESYNC_URL=https://scal.metacosmos.space etesync-dav &
-qsyncthingtray &
-nm-applet &
-# pasystray &
-# protonvpn-app &
-protonmail-bridge -n &
-# unclutter &
-# redshift-gtk &
--- .config/i3/local/example_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#xmodmap -e 'add mod3 = Menu'
-#compton --config $HOME/.config/compton.conf &
-#owncloud &
-#/usr/lib/notification-daemon-1.0/notification-daemon &
-#/HOME/.conkyconfig/conky_start &
-#cbatticon &
-# nm-applet &
-# blueman-applet &
